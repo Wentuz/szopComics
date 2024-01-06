f1d5dea623e59bfe4fb84b6df2da2eb05b3f60e4
Wentuziak <gabrielurbanski@wp.pl>
2024-01-06 17:17:19 +0100

    test lol

--- home/home.php
+++ home/home.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style/styleSheet.css">
+    <title>Home</title>
 </head>
 <body>
-    hello
+    <h1>Hello meow meow :3</h1>
 </body>
 </html>
